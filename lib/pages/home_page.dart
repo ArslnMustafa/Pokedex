97eb7e91ebb9958886626b3f39d42b28bdc4b475
Mustafa ARSLAN <alabula58@gmail.com>
2022-08-16 14:08:12 +0300

    main

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex/widgets/app_title.dart';
+import 'package:pokedex/widgets/pokemon_list.dart';
+import '../model/pokemon_model.dart';
+
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: OrientationBuilder(
+        builder: (context, orientation) => Column(
+          children: [
+            AppTitle(),
+            Expanded(child:  PokemonList()),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
